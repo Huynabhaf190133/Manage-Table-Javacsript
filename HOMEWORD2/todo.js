298b5caa00771055602f9d2a573ab77a2070bd84
Huynabhaf190133 <72755736+Huynabhaf190133@users.noreply.github.com>
2020-11-08 15:47:35 +0700

    a

--- HOMEWORD2/todo.js
+++ HOMEWORD2/todo.js
@@ -1,95 +1,97 @@
-// nút để add dữ liệu thông qua phương thức
-var addValue = document.getElementById("btn-Control-Add");
-// Đây là cách 1 để tạo table với phương thức đơn sơ và dài dòng
-function cTable() {
-  if (document.getElementById("input").value.trim() != "") {
-    document.getElementById("ms-log").style.display = "none";
-    var table = document.getElementById("table");
-    var tbody = document.getElementById("tbody");
-    var tr = document.createElement("tr");
-    var th = document.createElement("th");
-    var td = document.createElement("td");
-    var value = document.createTextNode(document.getElementById("input").value);
-    table.appendChild(tbody);
-    tbody.appendChild(tr);
-    tr.appendChild(td);
-    td.appendChild(value);
-  } else {
-    document.getElementById("ms-log").style.display = "block";
-  }
-}
-// End
-// ENd
 
-function insertTable() {
-  //get data from Input
-  // var name = document.getElementById("inputValue1").value;
-  // var status = document.getElementById("inputValue2").value;
-  // var time = document.getElementById("inputValue3").value;
-  //End of Input is disappear
-  var input = document.getElementById("input").value;
-  // if (name.trim() == "" || status.trim() == "" || time.trim() == "") {
-  //   alert("Vui lòng nhập đủ thông tin");
-  // }
-  if (input == "") {
-    document.getElementById("ms-log").style.display = "block";
-    input == "";
-  } else if (input.trim() == "") {
-    document.getElementById("ms-log").style.display = "block";
-    document.getElementById("ms-log").innerHTML = "You are kidding Me !!!";
-    input == "";
-  } else if (input.length <= 5) {
-    document.getElementById("ms-log").style.display = "block";
-    document.getElementById("ms-log").innerHTML = "Nhập trên 5 kí tự nhé !!!";
-    input == "";
-  } else {
-    document.getElementById("ms-log").style.display = "none";
-    if (document.getElementById("table") == undefined) {
-      createTable();
+var storageLocalget;
+// Make a Clock For site
+function MakeClock() {
+  setInterval(function () {
+    var time = new Date();
+    var h = time.getHours();
+    var m = time.getMinutes();
+    var s = time.getSeconds();
+    document.getElementById("clock").innerHTML =
+      replaceTime(h) + " : " + replaceTime(m) + " : " + replaceTime(s);
+  }, 1000);
+}
+function replaceTime(Time) {
+  return Time < 10 ? "0" + Time : Time;
+}
+MakeClock();
+
+// End of Clock
+
+// Get data From Dropdown
+
+function dropDown(status, e) {
+  document.getElementsByClassName("active")[0].classList.remove("active");
+  e.classList.add("active");
+  document.getElementById("btn-content").innerHTML = status;
+}
+
+// End of Get data From Dropdown
+
+// Function TO get
+function makeThis() {
+  var name = document.getElementById("input").value;
+  var status = document.getElementById("btn-content").innerText;
+  var time = document.getElementById("clock").innerText;
+  var cv = {
+    ten: name,
+    trangthai: status,
+    thoigian: time,
+  };
+  return cv;
+}
+// Create Local Storage
+var dsach=[];
+
+var checkLS=localStorage.getItem("danhsach");
+        if(checkLS != null){
+            dsach=[...JSON.parse(checkLS)];
+        }
+function localStorageData()
+{
+  localStorage.setItem("danhsach",JSON.stringify(dsach));
+}
+// Create
+function createTable()
+{
+dsach.push(makeThis());
+localStorageData();
+readTable();
+}
+// Read
+function readTable()
+{
+  var trData="";
+  var local=JSON.parse(localStorage.getItem("danhsach"));
+  
+  if(local!=null)
+  {
+    for(var i=0;i<local.length;i++)
+    {
+      trData+="<tr onclick='getDetail("+i+")'>"+"<td>"+local[i].ten+"</td>"+"<td>"+local[i].trangthai+"</td>"+"<td>"+local[i].thoigian+"</td>"+"</tr>";
     }
-    //get Element with ID is table
-    var table = document.getElementById("table");
-
-    // variable to create table
-    //Star
-    // var tr = "<th>" + name + "</th>";
-    // var th = "<td>" + status + "</td>";
-    // var td = "<td>" + time + "</td>";
-    //End
-    // innerHTML to show data
-    // table.innerHTML += "<tr>" + tr + th + td + "</tr>";
-    //ENd
-    document.getElementById("Done").innerHTML="<option>"+"Done"+"</option>";
-    table.innerHTML += "<tr>" + "<td>" + input + "</td>" + "</tr>";
-    return;
+    document.getElementById("tbody").innerHTML=trData;
   }
 }
+// Get Stt
+function getDetail(currentIndex)
+{
+  debugger
+  storageLocalget=currentIndex;
+  document.getElementById("input").value=dsach[currentIndex].ten;
+}
+readTable();
+function updateTable()
+{
+  dsach[storageLocalget]=makeThis();
+  localStorageData();
+  readTable();
+}
+// Delete
+function deleteData()
+{
+  dsach.splice(storageLocalget,1);
+  localStorageData();
+  readTable();
+}
 
-function createTable() {
-  var content = "";
-  var title1 = "";
-  var title2 = "";
-  var title3 = "";
-  title1 += "<th>" + "Name" + "</th>";
-  title2 += "<th>" + "Status" + "</th>";
-  title3 += "<th>" + "Time" + "</th>";
-  content +=
-    "<table class='table table-bordered table-hover' id='table'>" +
-    "<tr class='table-active'>" +
-    title1 +
-    title2 +
-    title3 +
-    "</tr>" +
-    "</table>";
-  document.getElementById("table-Own-Here").innerHTML = content;
-}
-// Event to Add Function
-addValue.addEventListener("click", insertTable);
-// Event to update table
-function uTable() {
-  var updateValue = document.getElementById("btn-Control-Update");
-}
-// Event to Edit table
-function eTable() {
-  var editValue = document.getElementById("btn-Control-Edit");
-}
